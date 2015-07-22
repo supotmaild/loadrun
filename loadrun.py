981414d5f476912cf849a27c48b194d4cbde9766
Supot Sawangpiriyakij <therithwarich@gmail.com>
2015-07-22 12:35:15 +0700

    Update loadrun.py

--- loadrun.py
+++ loadrun.py
@@ -1,23 +1,21 @@
 '''loadrun.py'''
 '''11 Jul 2015'''
-'''owner Supot Sawangpiriyakij'''
 '''one and only version'''
 global program_text
 program_text=[]
 def load(file_name):
 	global program_text
-	program_text = []
+	program_text=[]
 	f=open(file_name,'r')
-	read_text = f.readlines()
-	for line in read_text:
-		program_text.append(line)							
+	program_text = f.readlines()
 	f.close()
 def run():
 	global program_text
 	j = ''
 	for i in program_text:
 		if type(i) is str:
-			j = j + i + '\n'
+			j = j + i
 	if (j != ''):
 		exec(j + '\n')
 		j = ''
+
